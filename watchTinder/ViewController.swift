15418902c95d46e4f2066a885c857534b1b73508
Guillaume Hammadi <guillaume.hammadi@rea-group.com>
2015-02-12 16:34:33 +1100

    Fetch and display an image from the almighty Internet

--- watchTinder/ViewController.swift
+++ watchTinder/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var longitude : UILabel!
     @IBOutlet weak var latitude : UILabel!
+    @IBOutlet weak var propertyImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func sendHttpRequest(sender: AnyObject) {
+        let url = NSURL(string: "http://i4.au.reastatic.net/1010x570/ae1391f68762a68214342239581e3ade381554376684c119caf1bd25a6ccdcaf/main.jpg")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            (data, response, error) in
+                println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        }
+        
+        task.resume()
+        
+        let data = NSData(contentsOfURL: url!)
+        propertyImage.image = UIImage(data: data!)
     }
     
     // Delegates for CLLocationManager
