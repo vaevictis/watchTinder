37f85fedcc3fb480be4acd67a39a70e8655453bf
Guillaume Hammadi <guillaume.hammadi@rea-group.com>
2015-02-12 11:14:19 +1100

    Initial Commit

---
+++ watchTinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchTinder
+//
+//  Created by guillaume hammadi on 12/02/2015.
+//  Copyright (c) 2015 reaHack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
